9a3d32ca2d0a4a45df8ddbe085062bf9fdb6768d
Zhang <1152733386@qq.com>
2020-12-28 16:01:35 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,20 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication:Application() {
+    companion object {
+
+        const val TOKEN = "89f3efbd4aa183d495af2f94e8d9ef10" // 填入你申请到的令牌值
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
